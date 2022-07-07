630b499aa91c7294e8fa7df72127be681d8b8266
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-07-07 15:52:01 -0500

    [add] Registration UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
@@ -20,7 +22,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  formData = {
+    email: "",
+    password: ""
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRegister() {
+    console.log(this.formData);
   }
 
 }
