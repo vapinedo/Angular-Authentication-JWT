0bc2d766da0b57bf22a1e7fe678a982ea0b8ded3
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-07-07 15:25:58 -0500

    [add] Adding Components and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventsComponent } from './components/events/events.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SpecialEventsComponent } from './components/special-events/special-events.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/events', pathMatch: 'full'},
+  {path: 'events', component: EventsComponent},
+  {path: 'special', component: SpecialEventsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { EventsComponent } from './components/events/events.component';
+import { SpecialEventsComponent } from './components/special-events/special-events.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    EventsComponent,
+    SpecialEventsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
