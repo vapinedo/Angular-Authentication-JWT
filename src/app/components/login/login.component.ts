b6242fbb2800b946d3cb2fa0f3811e80ebad1b22
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-07-08 13:24:50 -0500

    [add] Storing JWT in the front-end

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,30 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   formData = {
-    email: "",
-    password: ""
+    email: '',
+    password: '',
   };
 
-  constructor(private authSvc: AuthService) { }
+  constructor(
+    private authSvc: AuthService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onLogin() {
-    this.authSvc.login(this.formData)
-      .subscribe({
-        next: data => console.log(data),
-        error: err => console.log(err),
-        complete: () => console.log('Completed login request')
-      })
+    this.authSvc.login(this.formData).subscribe({
+      next: (response) => {
+        console.log(response);
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/special']);
+      },
+      error: (err) => console.log(err),
+      complete: () => console.log('Completed login request'),
+    });
   }
-
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,30 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
   formData = {
-    email: "",
-    password: ""
+    email: '',
+    password: '',
   };
 
-  constructor(private authSvc: AuthService) { }
+  constructor(
+    private authSvc: AuthService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {  
-  }
+  ngOnInit(): void {}
 
   onRegister() {
-    this.authSvc.register(this.formData)
-      .subscribe({
-        next: data => console.log(data),
-        error: err => console.log(err),
-        complete: () => console.log('Complete register request')
-      })
+    this.authSvc.register(this.formData).subscribe({
+      next: (response) => {
+        console.log(response);
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['/special']);
+      },
+      error: (err) => console.log(err),
+      complete: () => console.log('Complete register request'),
+    });
   }
-
 }
